7c50168b04f86853c2168694577bab49fad5cd73
DritiDecani <ddecani@gmail.com>
2021-12-17 19:57:32 +0100

    POST Api per product schema me router

---
+++ server/routes/product.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const Product = require("../models/product");
+
+//POST
+router.post("/products",async (req, res) => {
+    try {
+      let product = new Product();
+      product.title = req.body.title;
+      product.description = req.body.description;
+      product.photo = req.body.photo;
+      product.stockQuantity = req.body.stockQuantity;
+
+      await product.save();
+
+      res.json({
+        status: true,
+        message: "Succesfully saved"
+      });
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: err.nessage
+      });
+    }
+  });
+
+//GET
+//GET single
+//PUT update
+//DELET
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -20,30 +20,19 @@ mongoose.connect(process.env.DATABASE,
   }
 );
 
+
 //Middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-//retrive
-app.get('/',(req , res) =>{
-    res.json("Hello amazon clone");
-});
-//send data from frontend to backend
-app.post("/" , (req, res)=>{
-    let user = new User();
-    user.name = req.body.name;
-    user.email = req.body.email;
-    user.password = req.body.password;
 
-    user.save((err => {
-        if(err) {
-            res.json(err);
-        }else {
-            res.json("Successfully saved");
-        }
-    }));
-});
+
+//Require Api
+const productRoutes = require('./routes/product');
+app.use("/api", productRoutes);
+
+
 
 app.listen(3000, err => {
     if (err) {
