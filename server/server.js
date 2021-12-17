5c3caa1ebffc047ddb20935c2de831f1e8ecb57b
DritiDecani <ddecani@gmail.com>
2021-12-17 16:31:55 +0100

    Add Mongoose + User.js

--- server/server.js
+++ server/server.js
@@ -9,10 +9,11 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+//retrive
 app.get('/',(req , res) =>{
     res.json("Hello amazon clone");
 });
-
+//send data from frontend to backend
 app.post("/" , (req, res)=>{
     console.log(req.body.name);
 
