be137d38c82ac1a22b361cf818172d0e4077de85
DritiDecani <ddecani@gmail.com>
2021-12-20 02:09:21 +0100

    Product API Get + Get single

--- server/routes/product.js
+++ server/routes/product.js
@@ -26,7 +26,36 @@ router.post("/products", upload.single("photo"), async (req, res) => {
 });
 
 //GET
+router.get("/products", async (req, res) => {
+  try {
+    let products = await Product.find();
+    res.json({
+      success: true,
+      products: products,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
 //GET single
+router.get("/products/:id", async (req, res) => {
+  try {
+    let product = await Product.findOne({_id:req.params.id});
+    res.json({
+      success: true,
+      product: product,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
 //PUT update
 //Delete
 module.exports = router;
