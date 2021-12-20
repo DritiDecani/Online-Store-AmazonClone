a2f77b0e3d94d8b6e83bf90fd0a9c496a3cc2249
DritiDecani <ddecani@gmail.com>
2021-12-20 02:40:33 +0100

    Product Update Api

--- server/routes/product.js
+++ server/routes/product.js
@@ -44,7 +44,7 @@ router.get("/products", async (req, res) => {
 //GET single
 router.get("/products/:id", async (req, res) => {
   try {
-    let product = await Product.findOne({_id:req.params.id});
+    let product = await Product.findOne({ _id: req.params.id });
     res.json({
       success: true,
       product: product,
@@ -57,5 +57,33 @@ router.get("/products/:id", async (req, res) => {
   }
 });
 //PUT update
+router.put("/products/:id", upload.single("photo"), async (req, res) => {
+  try {
+    let product = await Product.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $set: {
+          title: req.body.title,
+          price: req.body.price,
+          category: req.body.categoryID,
+          photo: req.file.location,
+          description: req.body.description,
+          owner: req.body.ownerID,
+        },
+      },
+      { upsert: true }
+    );
+
+    res.json({
+      success: true,
+      updatedProduct: product,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
 //Delete
 module.exports = router;
