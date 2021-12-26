68ab8918894c7e4578e514fe393a42e2b32eca12
coyg2000 <sr47050@ubt-uni.net>
2021-12-26 22:33:49 +0100

    Listing Page Part 1

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -1,25 +1,74 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">Admin</h1>
-      <h2 class="subtitle">My polished Nuxt.js project</h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="blank" class="button--grey">GitHub</a>
+<main>
+  <div class="a-spacing-large"></div>
+
+  <div class="container-fluid browsing-history">
+      <div class="row">
+          <div class="col-sm-8 col-8">
+              <h1 class="a-size-large a-spacing-none a-text-normal">All Products</h1>
+              <div class="a-spacing-large"></div>
+              <!--Button-->
+              <a href="#" class="a-button-buy-again">Add a new product</a>
+              <a href="#" class="a-button-history margin-right-10">Add a new category</a>
+              <a href="#" class="a-button-history margin-right-10">Add a new owner</a>
+
+          </div>
       </div>
-      
-      <a href="#" class="a-button-buy-again">Buy Now</a>
-    </div>
   </div>
+   <br>
+  <div class="container-fluid browsing-history">
+      <div class="row">
+          <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
+          <div class="history-box">
+              <!--Product image-->
+              <a href="#" class="a-link-normal">
+                  <img src="#" class="img-fluid">
+              </a>
+
+              <div class="a-spacing-top-base asin-title">
+                  <span class="a-text-normal">
+                      <div class="p13n-sc-truncated">Product title</div>
+                  </span>
+
+              </div>
+
+              <div class="a-row">
+                  <a href="#">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                  </a>
+                  <span class="a-letter-space"></span>
+                  <span class="a-color-tertiary a-size-small asin-reviews">(1732)</span>
+              </div>
+              <!--Product price-->
+
+              <div class="a-row">
+                  <span class="a-size-base a-color-price">
+                      <span class="p13n-sc-price">$23</span>
+                  </span>
+
+              </div>
+               <!--Product button-->
+               <div class="a-row">
+                   <a href="#" class="a-button-history margin-right-18">Update</a>
+                   <a href="#" class="a-button-history margin-right-18">Delete</a>
+
+               </div>
+
+          </div>
+        </div>
+      </div>
+  </div>
+</main>
 </template>
 
 <script>
-import Logo from "~components/Logo.vue";
+
 export default {
-  components: {
-    Logo
-  }
+ 
 };
 </script>
 
