4e5dbb0a49198bd3636c1bfd6fe8f84a0b2fcb24
jetmirgashi <jg48197@ubt-uni.net>
2021-12-31 17:59:56 +0100

    Featured Product

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,25 @@
 <template>
-<div class ="container">
-    <nuxt-link to ="/products" class ="a-button-buy-again">Add a new product</nuxt-link>
-</div> 
+  <main
+  components: { FeaturedProduct },ain class="listingPage">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xl-2 col-lg-3 md-4 col-sm-4">
+          <!-- Sidebar -->
+        </div>
+
+        <!-- Main content -->
+        <div class="col-xl-10 col-lg-9 md-8 col-sm-8"></div>
+        <FeaturedProduct />
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
+import FeaturedProduct from '~/components/FeaturedProduct.vue';
 export default {
-  name: 'IndexPage'
+  components: {
+    FeaturedProduct
+  }
 }
 </script>
