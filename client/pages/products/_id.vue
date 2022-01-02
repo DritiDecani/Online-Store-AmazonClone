6dda68d550ddaf1f59864316d2136f0a948c85ee
coyg2000 <sr47050@ubt-uni.net>
2022-01-03 00:00:30 +0100

    Pjesa per img dhe name of author

--- client/pages/products/_id.vue
+++ client/pages/products/_id.vue
@@ -20,5 +20,61 @@
 
             </div>
         </div>
+        <div class="container-fluid">
+            <div class="dp-container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-3 col-sm-4">
+                        <div class="leftCol">
+                            <!--Image-->
+                            <div class="imgBlock">
+                                <div class="eBooksimg">
+                                    <img src="/img/featuredProduct.jpg" class="img-fluid"/>
+                                </div>
+                            </div>
+
+                            <!--Follow Author-->
+                            <div class="authorFollow">
+                                <hr class="a-divider-normal"/>
+                                <h1 class="authorFollowHeading">Follow The Author</h1>
+                                <div class="a-spacing-top-small">
+                                    <div class="row">
+                                        <!--Author Image-->
+                                        <div class="col-xl-3 col-lg-3 col-md-3 col-cm-3 col-3">
+                                            <div class="smallAuthorImageContainer">
+                                                <a href="#">
+                                                    <img src="/img/featuredProduct.jpg" class="img-fluid"/>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <!--Author Data-->
+                                              <div class="col-xl-4 col-lg-3 col-md-3 col-cm-3 col-3">
+                                                <div class="authorNameCol">
+                                                    <a href="#">Filan Fisteku</a>
+                                                </div>
+
+                                              </div>
+                                               <div class="col-xl-5 col-lg-6 col-md-6 col-cm-6 col-6">
+                                                   <div class="authorBtn mt-2">
+                                                       <a href="#">
+                                                           <span class="btnFollow">
+                                                               <span class="a-btn-inner">
+                                                                   <button class="a-btn-text">+ Follow</button>
+                                                               </span>
+                                                           </span>
+                                                       </a>
+                                                   </div>
+                                               </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </main>
 </template>
