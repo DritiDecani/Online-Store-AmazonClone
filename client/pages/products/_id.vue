c4039eddc73ad62d4ebcfbfeeda064ceec9c0a09
coyg2000 <sr47050@ubt-uni.net>
2022-01-02 23:14:36 +0100

    Breadcrumbs apo shenimet tek faqja products/id

---
+++ client/pages/products/_id.vue
@@ -0,0 +1,24 @@
+<template>
+    <main>
+        <!-- Breadcrumbs-->
+        <div class="a-spacing-top-medium"></div>
+        <div class="container-fluid">
+            <div class="wayfinding-breadcrumbs-container">
+                <ul class="a-unordered-list a-horizontal a-size-small" >
+                    <li>
+                        <span class="a-list-item">
+                            <a class="a-link a-color-tertiary" href="#">Category</a>
+                        </span>
+                    </li>
+                     <li>
+                        <span class="a-list-item">
+                            <a class="a-link a-color-tertiary" href="#">Product Title</a>
+                        </span>
+                    </li>
+                 
+                </ul>
+
+            </div>
+        </div>
+    </main>
+</template>
