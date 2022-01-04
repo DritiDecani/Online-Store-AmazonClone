b6151ff40014f1dbfb3e06ae3338f63ebbfa4fc9
Besmir1 <bg38721@ubt-uni.net>
2022-01-04 22:24:50 +0100

    Product Page (Buying Secton)

--- client/pages/products/_id.vue
+++ client/pages/products/_id.vue
@@ -73,22 +73,21 @@
                             </div>
                         </div>
                     </div>
-                    
-                    <!-- Description  -->
+                    <!-- Middle 6 grid Description  -->
                     <div class="col-lg-6 col-md-6 col-sm-8"> 
                         <div class="centerCol">
                             <!-- Product Title -->
                             <div class="titleDiv">
                                 <h1 class="productTitle">
-                                    <span class="largeTitle">Albin Kurti</span>
-                                    <span class="smallTitle">Vetevendosje</span>
+                                    <span class="largeTitle">Filan Fisteku</span>
+                                    <span class="smallTitle">Description</span>
                                 </h1>
                             </div>
 
                             <!-- Aurthor's name-->
                             <div class="bylineinfo">
                                 by
-                                <a href="#" class="authorName">KGB Russia
+                                <a href="#" class="authorName">Anonymous
                                     <i class="fas fa-chevron-down" style="font-size: 8px !important; cloro: #555 !important;"></i>
                                 </a> (Author)
                             </div>
@@ -173,8 +172,8 @@
                                 <div class="bookDescriptionInner">This book is awesome</div>
                             </div>
                             <!-- Product Specification -->
-                            <div class="aboutEBooksfeature">
-                                <hr>
+                            <div class="aboutEbooksFeature">
+                                <hr />
                                 <div class="row">
                                     <div class="col-sm-4 mb-1">
                                         <div class="a-declarative">
@@ -183,6 +182,121 @@
                                     </div>
                                 </div>
                             </div>   
+                        </div>
+                    </div>
+                    <!-- Last 3 grid Buying section  -->
+                    <div class="col-lg-3 col-md-3 col-sm-6">
+                        <div class="combinedBuyBox">
+                            <div class="buyBox">
+                                <div class="a-section">
+                                    <div class="clearfix">
+                                        <div class="float-left">
+                                            <form>
+                                                <input type="radio" id="test1" name="radio-group checked" />
+                                                <label for="test1">Buy Now</label>
+                                            </form>
+                                        </div>
+
+                                        <!-- Product Price -->
+                                        <div class="float-right">
+                                            <span class="a-size-medium a-color-price offer-price a-text-normal">
+                                                $39
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="a-section a-spacing-none">
+                                    <div class="row">
+                                        <div class="col-sm-5 col-5">
+                                            <select>
+                                                <option value="1">Qty:&nbsp;1</option>
+                                                <option value="2">Qty:&nbsp;2</option>
+                                                <option value="3">Qty:&nbsp;3</option>
+                                                <option value="4">Qty:&nbsp;4</option>
+                                                <option value="5">Qty:&nbsp;5</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="a-section a-spacing-small a-spacing-top-micro">
+                                    <div class="row">
+                                        <span class="a-class-base buyboxShippingLabel"></span>
+                                    </div>
+                                </div>
+                                
+                                <div class="a-section a-spacing-small">
+                                    <div class="a-section a-spacing-none">
+                                        <span class="a-size-medium a-color-success">In Stock</span>
+                                    </div>
+                                    <div class="a-section a-spacing-mini">Ships from and sold by Amazone.com</div>
+                                </div>
+
+                                <div class="a-section">
+                                    <!-- Add to Cart - Button -->
+                                    <div class="a-button-stack">
+                                        <span class="a-spacing-small a-button-primary a-button-icon">
+                                            <span class="a-button-inner">
+                                                <i class="a-icon a-icon-cart"></i>
+                                                <input type="submit" name="submit.add-to-cart" class="a-button-input">
+                                                <span class="a-button-text">Add to Cart</span>
+                                            </span>
+                                        </span>
+                                    </div>
+                                    <!-- Buy Now - Button -->
+                                    <div class="a-button-stack">
+                                        <span class="a-spacing-small a-button-primary a-button-icon">
+                                            <span class="a-button-inner">
+                                                <i class="a-icon a-icon-buynow"></i>
+                                                <input type="submit" name="submit.add-to-cart" class="a-button-input">
+                                                <span class="a-button-text">Buy Now</span>
+                                            </span>
+                                        </span>
+                                    </div>
+                                </div>
+
+                                <div class="a-row">
+                                    <div class="a-spacing-top-small">
+                                        <div class="a-section a-spacing-none">
+                                            <div class="a-section a-spacing-none a-spacing-top-mini">
+                                                This item ships to
+                                                <b>Kosovo</b>
+                                                <b>Get it by Monday, Sept 23 - Monday, Sept. 30</b>
+                                                Choose this date at checkout
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <hr/>
+                                <span class="a-declarative">
+                                    <a href="#" class="a-link-normal">
+                                        <div class="a-row a-spacing-mini">
+                                            <i class="fal fa-map-market-alt"></i>
+                                            <span class="a-size-small">Deliver to Kosovo</span>
+                                        </div>
+                                    </a>
+                                </span>
+                                <br>
+                                <hr>
+                                <div class="a-section">
+                                    <div class="clearfix">
+                                        <div class="floar-left">
+                                            <form>
+                                                <input type="radio" name="radio-group" id="test2">
+                                                <label for="test1">Buy Now</label>
+                                            </form>
+                                        </div>
+                                        <div class="float-right">
+                                            <span class="a-color-base offer-price a-text-normal">$39</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="kcpAppBaseBox text-center">
+                            <img src="/img/readyondevice.png" class="img-fluid">
                         </div>
                     </div>
                 </div>
