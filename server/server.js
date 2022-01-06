25ec4c9dd358e324baf47919c02633e3508edd1b
DritiDecani <ddecani@gmail.com>
2022-01-06 18:36:58 +0100

    Signup API

--- server/server.js
+++ server/server.js
@@ -2,52 +2,50 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const dotenv = require('dotenv');
-const cors=require('cors');
-const User=require('./models/user');
+const dotenv = require("dotenv");
+const cors = require("cors");
+const User = require("./models/user");
 
 dotenv.config();
 
-const app = express()
+const app = express();
 
 mongoose.connect(
-    process.env.DATABASE,
-    {
-        useNewUrlParser:true,useUnifiedTopology:true
-
-    },
- (err) =>{
-    if (err){
-        console.log(err);
-    }else{
-        console.log("Connected to the Database")
+  process.env.DATABASE,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to the Database");
     }
   }
 );
 
-
 //Middlewares
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //Require Api
-const productRoutes = require('./routes/product');
-const categoryRoutes = require('./routes/category');
-const ownerRoutes = require('./routes/owner');
-
+const productRoutes = require("./routes/product");
+const categoryRoutes = require("./routes/category");
+const ownerRoutes = require("./routes/owner");
+const userRoutes = require("./routes/auth");
 
 app.use("/api", productRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", ownerRoutes);
+app.use("/api", userRoutes);
 
-app.listen(3000, err => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Listening on PORT", 3000);
-    }
+app.listen(3000, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Listening on PORT", 3000);
+  }
 });
