8b7170eba80be03f59b653505354e216e6f4b3ee
coyg2000 <sr47050@ubt-uni.net>
2022-01-10 00:54:38 +0100

    Get review Api

--- server/routes/review.js
+++ server/routes/review.js
@@ -33,6 +33,24 @@ if(savedReview){
 });
 
 //get
-
+router.get("/reviews/:productID",async(req,res)=>{
+try{
+    const productReviews=await Review.find({
+        productID:req.params.productID
+    })
+    .populate("user")
+    .exec();
+    
+    res.json({
+        success:true,
+        reviews:productReviews
+    });
+}catch(err){
+    res.status(500).json({
+        success: false,
+       message: err.message,
+});
+}
+});
 
 module.exports=router;
