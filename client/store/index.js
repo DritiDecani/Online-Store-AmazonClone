2aeaa990bd8dec5b06ee76081b7266459e1e3068
coyg2000 <sr47050@ubt-uni.net>
2022-01-16 20:52:49 +0100

    addProductToCart action

--- client/store/index.js
+++ client/store/index.js
@@ -4,3 +4,18 @@ export const state=()=>({
     cartLength:0
 
 })
+
+//state actions
+export const actions={
+    addProductToCart({state,commit},product){
+        const cartProduct=state.cart.find(prod=>prod._id==product._id);
+
+        if(!cartProduct){
+            commit("pushProductToCart",product);
+
+        }else{
+            commit("incrementProductQty",product)
+        }
+         commit("incrementCartLength");
+    }
+}
