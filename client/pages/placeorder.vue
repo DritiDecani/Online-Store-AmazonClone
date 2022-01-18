fb1621febd55dae51e0930f93692d02b2ccc21a3
jetmirgashi <jg48197@ubt-uni.net>
2022-01-18 16:52:25 +0100

    Creating placeorder.vue

---
+++ client/pages/placeorder.vue
@@ -0,0 +1,26 @@
+<template>
+<body>
+  <!--SHIPPING ADDRESS-->
+  <div class="container-fluid">
+    <div class="shipping-address">
+      <div class="navbarShipping a-spacing-large">
+        <nuxt-link to="/">
+          <img src="img/placeHeadernav.gif" class="img-fluid" />
+        </nuxt-link>
+      </div>
+      <div class="a-row">
+        <div class="a-size-large a-text-bold a-spacing-mini">Review your order</div>
+        <div class="a-row a-spacing-small a-size-mini"></div>
+      </div>
+      
+    </div>
+  </div>
+  <!--/SHIPPING ADDRESS-->
+</body>
+</template>
+
+<script>
+export default {
+  layout: "none"
+};
+</script>
