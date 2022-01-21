b5fa56272a0d3f9afb0f9d34d0f5b02427edd4e3
Besmir1 <bg38721@ubt-uni.net>
2022-01-21 01:28:13 +0100

    Order Page - API

--- server/models/order.js
+++ server/models/order.js
@@ -1,4 +1,5 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
+const deepPopulate = require("mongoose-deep-populate")(mongoose);
 const Schema=mongoose.Schema;
 
 const OrderSchema=new Schema({
@@ -13,5 +14,7 @@ products:[
 estimatedDelivery:String
 
 });
+
+OrderSchema.plugin(deepPopulate);
 
 module.exports=mongoose.model("Order",OrderSchema);
--- server/server.js
+++ server/server.js
@@ -39,6 +39,7 @@ const userRoutes = require("./routes/auth");
 const reviewRoutes = require("./routes/review");
 const addressRoutes = require("./routes/address");
 const paymentRoutes = require("./routes/payment");
+const orderRoutes = require("./routes/order");
 
 
 app.use("/api", productRoutes);
@@ -48,6 +49,7 @@ app.use("/api", userRoutes);
 app.use("/api",reviewRoutes);
 app.use("/api",addressRoutes);
 app.use("/api",paymentRoutes);
+app.use("/api",orderRoutes);
 
 
 app.listen(3000, (err) => {
